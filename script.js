4d7255160c0d44d209d7570758f064f0e1cb6965
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-07-15 18:48:26 +0400

    enabled adding book from the form

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-const newBookButton = document.getElementById("add-new-book");
+const openModalButton = document.getElementById("add-new-book");
+const closeModalButton = document.getElementById("cancel-button");
+const submitButton = document.getElementById("submit-button")
+const myModal = document.querySelector("dialog");
+const bookTitleDisplay = document.getElementsByClassName("book-title-display");
+const bookAuthorDisplay = document.getElementsByClassName("book-autor-display");
+const bookContainer = document.getElementById("library")
 const myLibrary = [];
 
-function Book(title, author, read) {
+function Book(title, author) {
     this.title = title;
     this.author = author;
-    this.read = read
+    // this.read = read
 };
 
-function addBookToLibrary(title, author, read) {
+function addBookToLibrary(title, author) {
     let id = crypto.randomUUID();
-    let newBook = new Book();
+    let newBook = new Book(title, author);
     myLibrary.push(newBook);
 }
 
 addBookToLibrary("harry potter", "j.k. rowling", "read");
-console.log(myLibrary)
+addBookToLibrary("twilight", "stephenie meyer", "read");
+addBookToLibrary("casual vacancy", "j.k. rowling", "read");
+console.log(myLibrary);
 
-newBookButton.addEventListener("click")
+openModalButton.addEventListener("click", () => myModal.showModal());
+
+let html = '';
+for (let i = 0; i < myLibrary.length; i++) {
+  html += `
+        <div class="book">
+            <p>${myLibrary[i].title}</p>
+            <p>${myLibrary[i].author}</p>
+        </div>`;
+}
+bookContainer.innerHTML = html;
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const bookTitleFinal = document.getElementById("book-title").value
+    const bookAuthorFinal = document.getElementById("book-author").value
+    addBookToLibrary(bookTitleFinal, bookAuthorFinal);
+    html = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        html += `
+                <div class="book">
+                    <p>${myLibrary[i].title}</p>
+                    <p>${myLibrary[i].author}</p>
+                </div>`;
+    }
+    bookContainer.innerHTML = html;
+});
+
